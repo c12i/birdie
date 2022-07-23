ecc921710092b507c78d63cedf77f58d70b8430a
Collins Muriuki <hello@collinsmuriuki.xyz>
2022-07-23 06:19:26 +0300

    Remove spaces module

--- twitter_api/src/main.rs
+++ twitter_api/src/main.rs
@@ -1,18 +1,9 @@
 mod lists_api;
-mod spaces_api;
 mod tweet_api;
-
-use spaces_api::SpacesApi;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
-
-    let api = SpacesApi::new().await?;
-
-    let space = api.get_space("1zqKVXPQhvZJB".to_string()).await?;
-
-    println!("{}", space.id);
 
     Ok(())
 }
